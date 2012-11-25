b35a332d144b113ca720e293cd3caf7cf1b440ee
Andy G <tabete@gmail.com>
2012-11-25 15:56:41 -0500

    upping a working version of torhound

--- torhound.py
+++ torhound.py
@@ -1,33 +1,49 @@
-# Parse an apache log file and checks for tor exit nodes.
-# Takes a user specified action if one is found.
-# https://trac.torproject.org/projects/tor/wiki/doc/TorDNSExitList
-# http://code.google.com/p/apachelog/downloads/list
+# Parse an apache acess log and checks for tor exit nodes.
+# TODO: add hidemyass.com check
 
-import urllib.request
-import string
+import fileinput
 import re
+import string
+import sys
+import urllib.request
 
-url = "http://exitlist.torproject.org/exit-addresses"
-exits = urllib.request.urlopen(url).read()
-exitlist = []
+def getTorExits():
+	# https://trac.torproject.org/projects/tor/wiki/doc/TorDNSExitList
+	# http://code.google.com/p/apachelog/downloads/list
+	url = "http://exitlist.torproject.org/exit-addresses"
+	exits = urllib.request.urlopen(url).read()
+	exitlist = []
 
-for exitnode in str( exits, encoding='utf8' ).split('\n'):
-    if "ExitAddress" in exitnode:
-        exitlist.append(exitnode.split(' ')[1])
+	for exitnode in str( exits, encoding='utf8' ).split('\n'):
+		if "ExitAddress" in exitnode:
+			exitlist.append(["Tor Exit", exitnode.split(' ')[1]])
 
-print(len(exitlist))
+	return exitlist
+	
+def getHMAProxies():
+	return []
+	
+def printProxies(prox): 
+	for node in proxies:
+		print(node)
+	print(len(proxies))
 
+def getAddressFromLog(line):
+	ip = re.match("^\d+\.\d+\.\d+\.\d+", line)
+	return ip.group(0).strip()
 
-rexp = re.compile('\d+\.\d+\.\d+\.\d+')
-a = rexp.match(line)
-if not a is None:
-a.group(1) #IP address
+if len(sys.argv) != 2 :
+	print("Usage: python torhound.py ACCESS_LOG")
+	sys.exit()
 
+proxies = []
+proxies += getTorExits()
+proxies += getHMAProxies()
 
-for line in open('/var/log/httpd/access.log','r'):
-    doSomething(line
+#printProxies()
 
-f = open('/tmp/workfile', 'w')
-for line in f:
-
-f.close()
+for line in fileinput.input(sys.argv[1]):
+	ip = getAddressFromLog(line)
+	for prox in proxies:
+		if ip == prox[1]:
+			print(prox[0] + " found: " + prox[1])
