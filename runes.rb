fa18fd5accf77e46466f6d83527e1795b5db46b9
vxhex <tabete@gmail.com>
2014-05-19 21:39:42 -0500

    Newline sadness abounds

--- runes.rb
+++ runes.rb
@@ -4,10 +4,11 @@ runes_to_show = 3
 
 runes = ['ᚠ', 'ᚢ', 'ᚦ', 'ᚨ', 'ᚱ', 'ᚲ', 'ᚷ', 'ᚹ', 'ᚺ', 'ᚾ', 'ᛁ', 'ᛃ', 'ᛇ', 'ᛈ', 'ᛉ', 'ᛊ', 'ᛏ', 'ᛒ', 'ᛖ', 'ᛗ', 'ᛚ', 'ᛝ', 'ᛟ', 'ᛞ']
 
-print "
+print '
 +------------------+
 | Runes of the Day |
-+------------------+\n"
++------------------+
+'
 
 (1 .. runes_to_show).each do
 	print "#{runes.delete_at(rand(runes.length))}  "
